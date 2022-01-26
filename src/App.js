960fdb689667c42e786666bee33904ba295d7a4d
정재호 <b8739@hanmail.net>
2022-01-26 13:02:25 +0900

    Change Parent Component's state dynamically

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import Clothes from 'components/Clothes.js'
 import './App.css'
 
 function App() {
+    const [modalStatus, changeModalStatus] = useState(false)
+    const [modalValue, changeModalValue] = useState('')
+
     return (
         <div className="container">
             <div className="navBar">개발 blog</div>
-            <Clothes></Clothes>
+            <Clothes
+                changeModalValue={changeModalValue}
+                changeModalStatus={changeModalStatus}
+            ></Clothes>
+            <button
+                onClick={() => {
+                    changeModalStatus(!modalStatus)
+                }}
+            >
+                모달 보여주기
+            </button>
+
+            {modalStatus === true ? <Modal modalValue={modalValue} /> : null}
         </div>
     )
 }
+function Modal(props) {
+    return (
+        <div className="modal">
+            <h1>Modal</h1>
 
+            <h2>{props.modalValue}</h2>
+        </div>
+    )
+}
 export default App
--- src/components/Clothes.js
+++ src/components/Clothes.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Clothes = () => {
+const Clothes = (props) => {
     const [clothes, setClothes] = useState(['맨투맨', '청바지', '반지'])
     const sold = () => {
         alert('sold')
@@ -8,12 +8,19 @@ const Clothes = () => {
     return (
         <div>
             <div className="productList">
-                {clothes.map((a) => {
+                {clothes.map((cloth) => {
                     return (
-                        <div>
-                            <h3> {a}</h3>
+                        <div
+                            className="product"
+                            onClick={() => {
+                                props.changeModalValue(cloth)
+                                props.changeModalStatus(true)
+                            }}
+                        >
+                            <h3> {cloth}</h3>
                             <p>1월 25일 발행</p>
                             <button onClick={sold}>판매 완료</button>
+
                             <hr />
                         </div>
                     )
